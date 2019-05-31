804f7b53ec06bbe6822060decfdabac3b047f47f
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-05-31 11:11:18 +0530

    Create demo.sql

---
+++ demo.sql
@@ -0,0 +1,5 @@
+INSERT INTO table_listnames (name, address, tele)
+SELECT * FROM (SELECT 'Rupert', 'Somewhere', '022') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM table_listnames WHERE name = 'Rupert'
+) LIMIT 1;
